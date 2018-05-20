febbf7a6f8b87cb88ca8d710e4bc09a754d753eb
Nick Machairas <nick@machairas.com>
2018-05-20 18:57:27 -0400

    rename to 'particle_analyzer'

--- examples/sample_analysis.py
+++ examples/sample_analysis.py
@@ -1,6 +1,6 @@
-# from .context import qicpicxml
+# from .context import particle_analyzer
 
-from qicpicxml.core import say_hello
+from particle_analyzer.core import say_hello
 
 if __name__ == "__main__":
     say_hello()
