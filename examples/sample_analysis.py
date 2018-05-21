c723b074694da949cec49efeb3f540c3ff6d36f1
Nick Machairas <nick@machairas.com>
2018-05-20 23:00:27 -0400

    testing the core algorithms

--- examples/sample_analysis.py
+++ examples/sample_analysis.py
@@ -1,6 +1,7 @@
 # from .context import particle_analyzer
 
-from particle_analyzer.core import say_hello
+from particle_analyzer.core import ParticleData
+
 
 if __name__ == "__main__":
-    say_hello()
+    test1 = ParticleData('20180501_120357_001.xml')
--- particle_analyzer/core.py
+++ particle_analyzer/core.py
@@ -1,2 +1,57 @@
-def say_hello():
-    print("Hello World!")
+import xml.etree.ElementTree as ETree
+
+
+class ParticleData(object):
+    """Class to represent a particle data set
+
+    Attributes
+    ----------
+    file_path : str
+        The path to the XML file
+
+    Methods
+    -------
+    parse_file()
+        Parses input file and saves values in a Pandas data frame
+    """
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.date, self.product, self.filter, self.particle_num = \
+            self.parse_file()
+
+    def parse_file(self):
+        """Parses input file and saves values in a Pandas data frame
+
+        Returns
+        -------
+        date : str
+            The date and time that the experiment was performed
+            TODO: check if this is the date of data export instead
+        product : str
+            The project name, which is usually the sample name
+        filter : str
+            TODO: find the proper definition
+        particle_num : int
+            Total number of particles within the file
+        DataFrame
+            A Pandas data frame with all values from the XML input file
+        """
+
+        print("Parsing input file...")
+        tree = ETree.parse(self.file_path)
+        root = tree.getroot()
+        date = root[0].attrib['measurement']
+        product = root[0].attrib['product']
+        test_filter = root[0].attrib['filter']
+        particle_num = len(root) - 1
+
+        print("File Header")
+        print("-----------")
+        print("Date:", date)
+        print("Product:", product)
+        print("Filter:", test_filter)
+        print("Detected data for {} particles within {}".format(particle_num,
+                                                                self.file_path))
+
+        return date, product, test_filter, particle_num
