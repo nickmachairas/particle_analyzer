2ff24bf821396ffc8a83381ef8f9ea10b3230c43
Nick Machairas <nick@machairas.com>
2018-05-21 23:21:36 -0400

    further testing

--- examples/sample_analysis.py
+++ examples/sample_analysis.py
@@ -5,3 +5,10 @@ from particle_analyzer.core import ParticleData
 
 if __name__ == "__main__":
     test1 = ParticleData('20180501_120357_001.xml')
+    print(test1.df.head())
+    print(test1.df.info())
+    print(test1.df.describe())
+    test2 = ParticleData('ottawa sand 2030.xml')
+    print(test2.df.head())
+    print(test2.df.info())
+    print(test2.df.describe())
